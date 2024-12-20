71af5ac1373a90a220512b2984ef782486a57ca8
Akihiko Tomokane <mpc9000@gmail.com>
2024-12-20 16:57:53 +0900

    Create button using pressable

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,45 +1,54 @@
-import { Text, View , StyleSheet} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 
+import Button from "@/components/button";
 import ImageViewer from "@/components/imageViewer";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
-  return (
-			<View style={styles.container}>
-				<View style={styles.imageContainer}>
-          <ImageViewer imgSource={PlaceholderImage} />
-        </View>
-				<Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
-				<Link href="/about" style={styles.button}>
-					Go to About
-				</Link>
+	return (
+		<View style={styles.container}>
+			<View style={styles.imageContainer}>
+				<ImageViewer imgSource={PlaceholderImage} />
 			</View>
-		);
+			<View style={styles.footerContainer}>
+				<Button label="Choose a photo" />
+				<Button label="Use this photo" />
+			</View>
+			<Text style={styles.text}>Edit app/index.tsx to edit this screen.</Text>
+			<Link href="/about" style={styles.button}>
+				Go to About
+			</Link>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  imageContainer: {
-    flex: 1,
-  },
-  text: {
-    color: "#f00",
-  },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: "underline",
-    color: "#0ff",
-  }
+	container: {
+		flex: 1,
+		backgroundColor: "#25292e",
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	imageContainer: {
+		flex: 1,
+	},
+	text: {
+		color: "#f00",
+	},
+	image: {
+		width: 320,
+		height: 440,
+		borderRadius: 18,
+	},
+	button: {
+		fontSize: 20,
+		textDecorationLine: "underline",
+		color: "#0ff",
+	},
+	footerContainer: {
+		flex: 1 / 3,
+		alignItems: "center",
+	},
 });
